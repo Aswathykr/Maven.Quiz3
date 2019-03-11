dcf3c529da821458a1691e7828c71e45cb49c187
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-11 10:53:18 -0400

    collection

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -1,17 +1,38 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import rocks.zipcode.io.quiz3.objectorientation.enums.LabStatus;
+
+import java.util.Objects;
+
 /**
  * @author leon on 10/12/2018.
  */
-public class Lab {
+public class Lab implements Comparable {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lab)) return false;
+        Lab lab = (Lab) o;
+        return Objects.equals(labName, lab.labName);
+    }
+
+    String labName;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.labName = labName;
     }
 
     public String getName() {
-        return null;
+        return labName;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Lab lab = (Lab) o;
+        return this.getName().compareTo(lab.getName());
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -2,31 +2,60 @@ package rocks.zipcode.io.quiz3.collections;
 
 import rocks.zipcode.io.quiz3.objectorientation.enums.LabStatus;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+    Map<Lab, LabStatus> labStatusMap = new HashMap<>();
+    Map<String, Lab> labMap = new HashMap<>();
+
+    @Override
+    public String toString() {
+        String result = "";
+        Lab[] labArray = labStatusMap.keySet().toArray(new Lab[labStatusMap.keySet().size()]);
+
+        Arrays.sort(labArray);
+        for(Lab lab : labArray){
+            result = result + lab.getName() + " > " + labStatusMap.get(lab)+ "\n";
+        }
+        return result.substring(0, result.length()-1);
+    }
+
     public Student() {
         this(null);
     }
 
     public Student(Map<Lab, LabStatus> map) {
+        if(map != null)
+           this.labStatusMap = map;
     }
 
     public Lab getLab(String labName) {
-        return null;
+
+        return labMap.get(labName);
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        Lab lab = labMap.getOrDefault(labName, null);
+        if(lab == null){
+            throw new UnsupportedOperationException();
+        }
+        labStatusMap.put(lab, labStatus);
     }
+
 
 
     public void forkLab(Lab lab) {
+
+        labMap.put(lab.getName(), lab);
+        labStatusMap.put(lab, LabStatus.PENDING);
     }
 
     public LabStatus getLabStatus(String labName) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        Lab lab = labMap.getOrDefault(labName, new Lab(labName));
+
+        return labStatusMap.get(lab);
     }
 }
