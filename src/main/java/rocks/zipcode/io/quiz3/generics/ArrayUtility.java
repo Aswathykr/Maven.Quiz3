416463c28ea31d11782fffd74608f64de66ecddc
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-11 11:13:15 -0400

    generics

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,6 +1,12 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * @author leon on 09/12/2018.
@@ -13,18 +19,30 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType findOddOccurringValue() {
-        return null;
+
+        return Arrays.stream(array)
+                .filter(value -> getNumberOfOccurrences(value) % 2 != 0)
+                .findFirst().get();
     }
 
     public SomeType findEvenOccurringValue() {
-        return null;
+        return Arrays.stream(array)
+                .filter(value -> getNumberOfOccurrences(value) % 2 == 0)
+                .findFirst().get();
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        return Arrays.stream(array)
+                .filter(value -> value == valueToEvaluate)
+                .collect(Collectors.toList()).size();
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        return null;
+
+        List<SomeType> list = Arrays.stream(array)
+                .filter(value -> predicate.apply(value))
+                .collect(Collectors.toList());
+        ;
+        return list.toArray((SomeType[])Array.newInstance(array[0].getClass(),list.size()));
     }
 }
