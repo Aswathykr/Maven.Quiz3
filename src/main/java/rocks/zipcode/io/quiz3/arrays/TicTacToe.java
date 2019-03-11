f44ffb559f2bc35984508f494604d0b596e72b0a
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-11 10:10:18 -0400

    arrays

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -1,34 +1,65 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
 
+    private String board[][];
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        return Arrays.stream(board)
+                .map(row -> row[value])
+                .toArray(String[]::new);
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+
+        return Arrays.stream(board[rowIndex])
+                .allMatch(str -> str.equals(board[rowIndex][0]));
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        String[] column = getColumn(columnIndex);
+        return Arrays.stream(column)
+                .allMatch(str -> str.equals(column[0]));
     }
 
     public String getWinner() {
-        return null;
+        String winner = null;
+
+        if(board[0][0].equals(board[1][1]) &&
+                board[0][0].equals(board[2][2])){
+            winner = board[0][0];
+        }
+        else if(board[0][2].equals(board[1][1]) &&
+                board[0][2].equals(board[0][2])){
+            winner = board[0][2];
+        }
+        else{
+            for (int i = 0; i < board.length; i++) {
+                Boolean rowEqual = isRowHomogenous(i);
+                Boolean columnEqual = isColumnHomogeneous(i);
+                if(rowEqual || columnEqual){
+                    winner = board[i][0];
+                    break;
+                }
+            }
+        }
+
+        return winner;
     }
 
     public String[][] getBoard() {
--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,32 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        if(str.length() > 0) {
+
+            String lowercase = str.toLowerCase();
+
+            if(Character.isLetter(lowercase.charAt(0))) {
+                result.add(Character.toUpperCase(lowercase.charAt(0)) +
+                        lowercase.substring(1));
+            }
+            for (int i = 1; i < str.length(); i++) {
+                char chAtIdx = lowercase.charAt(i);
+                if(Character.isLetter(chAtIdx)) {
+                    result.add(lowercase.substring(0, i) +
+                            Character.toUpperCase(chAtIdx) +
+                            lowercase.substring(i + 1));
+                }
+            }
+        }
+        return result.toArray(new String[result.size()]);
     }
 }
